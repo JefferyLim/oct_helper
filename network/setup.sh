cb2c223d5cd89bbf7ce39f436907c3201ca4603c
JefferyLim <jjlim@bu.edu>
2025-06-13 16:06:03 -0400

    Fixing ip

--- network/setup.sh
+++ network/setup.sh
@@ -4,5 +4,5 @@ sudo ip link set enp59s0np1 netns hostns
 sudo ip netns exec hostns ip addr add 192.168.40.20/24 dev enp59s0np1
 sudo ip netns exec hostns ip link set enp59s0np1 up
 
-sudo ip addr add 192.168.40.20/24 dev enp59s0np0 
+sudo ip addr add 192.168.40.10/24 dev enp59s0np0 
 sudo ip link set enp59s0np0 up

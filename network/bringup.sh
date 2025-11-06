419fcd0eab3d84ccd053fb12aa995bf24b94a591
Jeffery Lim <eng@jefferyjlim.com>
2025-11-06 13:52:36 -0500

    Adding defaults to bringup

--- network/bringup.sh
+++ network/bringup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo ip addr add $1/24 dev enp59s0np$2
-sudo ip link set enp59s0np$2 up
-sudo ip link set dev enp59s0np$2 mtu 9000
+IP=${1:-192.168.50.10}
+IF=${2:0}
+
+sudo ip addr add $IP/24 dev enp59s0np$IF
+sudo ip link set enp59s0np$IF up
+sudo ip link set dev enp59s0np$IF mtu 9000

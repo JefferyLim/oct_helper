6e77bd36d1fc539685c588bc9acddaa1580b2f13
Jeffery Lim <eng@jefferyjlim.com>
2025-11-06 13:46:48 -0500

    Adding file parameter for reprogram

--- fpga/reprogram.sh
+++ fpga/reprogram.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+MCS_FILE=${1:-fpga.mcs}
+
+
 config-fpga reset ~/private_key.pem
 echo Y | sudo xbflash2 program --spi --image ~/fpga.mcs -d 3b:00.0 --bar 2
 config-fpga boot ~/private_key.pem

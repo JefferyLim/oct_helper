aecf8a0eb74c0de292a202d365b003de01b148b8
Jeffery Lim <eng@jefferyjlim.com>
2026-02-28 03:22:27 -0500

    Adding the vm

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 
-MCS="${1:-fpga.mcs}"
-TARGET="${2:-pc159}"
+TARGET="${1:-pc159}"
 
 echo $TARGET
 
-rsync -avz private_key.pem "$MCS" ubuntu22.04.qcow2 $TARGET:~/.
+rsync -avz ~/projects/latest_working_OS4C $TARGET:~/.
+rsync -avz private_key.pem corundum.mcs ubuntu22.04.qcow2 $TARGET:~/.
